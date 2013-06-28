b5b3b5cffb169069c48e8465963906f3a729e308
kevinwinata <kevinwinata@gmail.com>
2013-06-28 08:49:53 +0700

    Define initial ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,4 +1,44 @@
 class Ability
 	include CanCan::Ability
 	
+	def initialize(user)
+		if user.role == '0'
+			can :manage, :all
+		elsif user.role == '1'
+			can :read, Company do |company|
+				Ownership.find_by_owner_id(user).company_id == company.id
+			end
+			can :update, Company do |company|
+				Ownership.find_by_owner_id(user).company_id == company.id
+			end
+			can :read, Billing do |billing|
+				Ownership.find_by_owner_id(user).company_id == billing.company_id
+			end
+			can :manage, Office do |office|
+				Ownership.find_by_owner_id(user).company_id == office.company_id
+			end
+			can :manage, User do |employee|
+				Ownership.find_by_owner_id(user).company_id == employee.company_id
+			end
+			cannot :change_status, Company
+		elsif user.role == '2'
+			can :manage, User do |employee|
+				Ownership.find_by_owner_id(user).company_id == employee.company_id
+			end
+			cannot :change_role, User
+			can :manage, Worktime do |worktime|
+				worktime.user_id = user.id
+			end
+		else
+			can :read, User do |employee|
+				user == employee
+			end
+			can :update, User do |employee|
+				user == employee
+			end
+			can :manage, Worktime do |worktime|
+				worktime.user_id = user.id
+			end
+		end
+	end
 end
