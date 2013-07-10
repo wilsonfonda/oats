0cf717dfe350e8597eecd7317b87d8481e3c1601
kevinalfianto <kevin_alfianto@yahoo.com>
2013-07-10 16:11:30 +0700

    sort user query by id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 	
 	def index
 		@offices = Company.find(current_user.company_id).offices
-		@users = User.where("office_id IN (?)",@offices).paginate(:page => params[:page], :per_page => 20)
+		@users = User.where("office_id IN (?)",@offices).order("id").paginate(:page => params[:page], :per_page => 20)
 	end
 
 	def add
