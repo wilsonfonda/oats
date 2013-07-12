abb145bb4abcb4f2155e182f0d360adf2cbc1063
kevinwinata <kevinwinata@gmail.com>
2013-07-12 10:46:32 +0700

    Magic grid AJAX - incomplete

--- app/models/worktime.rb
+++ app/models/worktime.rb
@@ -1,7 +1,7 @@
 class Worktime < ActiveRecord::Base
   attr_accessible :checkin, :checkout, :place_checkin, :place_checkout
   belongs_to :user
-
+  
   validates :user_id, presence: true
   validates :place_checkin, length: { maximum: 50 }
   validates :place_checkout, length: { maximum: 50 }
